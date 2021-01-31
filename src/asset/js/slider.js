f30c9807d1b50ba6bec0a797350df5e646f194fb
jojojo0501 <sarukiti0418@outlook.jp>
2021-01-31 21:56:45 +0900

    add slider

--- src/asset/js/slider.js
+++ src/asset/js/slider.js
@@ -1,1 +1,27 @@
 // 画像スライダーの実装はこちらに記述してください
+$(function(){
+  //スライドの画像の数を取得
+$('.p-top-slider').each(function(){
+  let $slides = $(this).find('.slider-item');
+  let slideNum = $slides.length;
+  let currentIdx = 0;　//何番目か
+
+  //最初の画像をフェードイン
+  $('.slider-item').eq(currentIdx).fadeIn();
+  //３秒後に次のスライドを表示
+  setInterval(NextSlide,3000);
+  
+  //次のスライドを表示するメソッド
+  function NextSlide(){
+    let nextIdx = currentIdx + 1;
+    //最後のスライドの場合は最初に戻る
+    if (nextIdx > (slideNum - 1)){
+      nextIdx = 0
+    }
+    //現在のスライドをフェードアウト
+    $('.slider-item').eq(currentIdx).fadeOut();
+    $('.slider-item').eq(nextIdx).fadeIn();
+    currentIdx = nextIdx;
+  }
+});
+});
