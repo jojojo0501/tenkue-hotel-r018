b476870b06b67b1565bf1d6f6779297f36ca3fed
nitro583 <seiyamatsumoto@me.com>
2021-02-01 00:38:47 +0900

    depeloped FAQ

--- src/asset/js/accodion.js
+++ src/asset/js/accodion.js
@@ -1,1 +1,22 @@
 // アコーディオンの実装はこちらに記述してください
+
+// $(function(){
+//   $('.js-menu__item__link').each(function(){
+//       $(this).on('click',function(){
+//           $("+.submenu",this).slideToggle(200);
+//           $(this).toggleClass('open', 200);
+//           return false;
+//       });
+//   });
+// });
+
+jQuery(function ($) {
+  $('.js-menu__item__link').on('click', function () {
+    /*クリックでコンテンツを開閉*/
+    $(this).next().slideToggle(300);
+    /*矢印の向きを変更*/
+    $(this).toggleClass('open', 0);
+    $(this).next().toggleClass('open', 0);
+    return false;
+  });
+  });
