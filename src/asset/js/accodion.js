d214424668bc49e227f8a3aa6c9495169aadd133
nitro583 <seiyamatsumoto@me.com>
2021-02-01 13:06:49 +0900

    SCSSのクラス名を命名規則に沿って変更、アコーディオンメニューの微調整

--- src/asset/js/accodion.js
+++ src/asset/js/accodion.js
@@ -1,22 +1,16 @@
 // アコーディオンの実装はこちらに記述してください
 
-// $(function(){
-//   $('.js-menu__item__link').each(function(){
-//       $(this).on('click',function(){
-//           $("+.submenu",this).slideToggle(200);
-//           $(this).toggleClass('open', 200);
-//           return false;
-//       });
-//   });
-// });
 
 jQuery(function ($) {
-  $('.js-menu__item__link').on('click', function () {
+  $('.js-accordionLink').on('click', function () {
     /*クリックでコンテンツを開閉*/
-    $(this).next().slideToggle(300);
+    $(this).next().slideToggle(100);
+    $(this).next().children().fadeToggle(100);
     /*矢印の向きを変更*/
     $(this).toggleClass('open', 0);
     $(this).next().toggleClass('open', 0);
+    $(this).next().children().toggleClass('open', 0);
+
     return false;
   });
   });
