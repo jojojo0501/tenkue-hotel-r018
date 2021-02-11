fef2ad5223db5a10a93bad102921fc3477dd0325
oga-0927 <totti0927.oga@gmail.com>
2021-02-11 10:42:51 +0900

    areaguide

--- src/asset/js/modal.js
+++ src/asset/js/modal.js
@@ -1,14 +1,13 @@
-$( '.js-modal-opener' ).each( function() {
-  $( this ).on( 'click', function() {
-       var target = $( this ).data( 'target' );
-       var modal = document.getElementById( target );
-       $( modal ).fadeIn( 300 );
-       return false;
+$('.js-modal-opener').each(function() {
+  $(this).on('click', function() {
+      var target = $(this).data('target');
+      var modal = document.getElementById(target);
+      $(modal).fadeIn(300);
+      return false;
   });
 });
 
-// ウィンドウを閉じる
 $( '.js-modal-closer' ).on( 'click', function() {
- $( '.js-modal-element' ).fadeOut( 300 );
- return false;
+  $( '.js-modal-element' ).fadeOut( 300 );
+  return false;
 });
