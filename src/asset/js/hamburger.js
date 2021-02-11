50c1957fb0fabff4f411bfd27414a79ce32b25bf
oga-0927 <totti0927.oga@gmail.com>
2021-02-11 16:21:31 +0900

    レスポンシブ修正

--- src/asset/js/hamburger.js
+++ src/asset/js/hamburger.js
@@ -1,9 +1,3 @@
-// ハンバーガーメニューの実装はこちらに記述してください
-// $('.js-button-Hamburger').on('click', () => {
-//   $('js-button-Hamburger').toggleClass('close');
-//   $('js-nav-cover').fadeToggle(500);
-// });
-
 $('.c-burger__btn').on('click',function(){
 $('body').toggleClass('open');
 $('.l-nav__wrapper').fadeToggle(500);
