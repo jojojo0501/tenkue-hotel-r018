695579d1f65a6720f886c2a001fd35b9282660b8
nitro583 <seiyamatsumoto@me.com>
2021-02-16 23:28:01 +0900

    アコーディオン動作に関わるクラス名をopenからis-openに変更、 jQueryのCDNを削除

--- src/asset/js/accodion.js
+++ src/asset/js/accodion.js
@@ -7,9 +7,9 @@ jQuery(function ($) {
     $(this).next().slideToggle(100);
     $(this).next().children().fadeToggle(100);
     /*矢印の向きを変更*/
-    $(this).toggleClass('open', 0);
-    $(this).next().toggleClass('open', 0);
-    $(this).next().children().toggleClass('open', 0);
+    $(this).toggleClass('is-open', 0);
+    $(this).next().toggleClass('is-open', 0);
+    $(this).next().children().toggleClass('is-open', 0);
 
     return false;
   });
