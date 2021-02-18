56dc7199c465467c15e0edd9f7126eb60d43016c
jojojo0501 <sarukiti0418@outlook.jp>
2021-02-18 21:46:25 +0900

    インデント修正

--- src/asset/js/slider.js
+++ src/asset/js/slider.js
@@ -1,27 +1,27 @@
 // 画像スライダーの実装はこちらに記述してください
 $(function(){
   //スライドの画像の数を取得
-$('.p-top__slider').each(function(){
-  let $slides = $(this).find('.p-top__slider__item');
-  let slideNum = $slides.length;
-  let currentIdx = 0;　//何番目か
+  $('.p-top__slider').each(function(){
+    let $slides = $(this).find('.p-top__slider__item');
+    let slideNum = $slides.length;
+    let currentIdx = 0;　//何番目か
 
-  //最初の画像をフェードイン
-  $('.p-top__slider__item').eq(currentIdx).fadeIn(2000);
-  //３秒後に次のスライドを表示
-  setInterval(NextSlide,4000);
-  
-  //次のスライドを表示するメソッド
-  function NextSlide(){
-    let nextIdx = currentIdx + 1;
-    //最後のスライドの場合は最初に戻る
-    if (nextIdx > (slideNum - 1)){
-      nextIdx = 0
+    //最初の画像をフェードイン
+    $('.p-top__slider__item').eq(currentIdx).fadeIn(2000);
+    //３秒後に次のスライドを表示
+    setInterval(NextSlide,4000);
+    
+    //次のスライドを表示するメソッド
+    function NextSlide(){
+      let nextIdx = currentIdx + 1;
+      //最後のスライドの場合は最初に戻る
+      if (nextIdx > (slideNum - 1)){
+        nextIdx = 0
+      }
+      //現在のスライドをフェードアウト
+      $('.p-top__slider__item').eq(currentIdx).fadeOut(2000);
+      $('.p-top__slider__item').eq(nextIdx).fadeIn(2000);
+      currentIdx = nextIdx;
     }
-    //現在のスライドをフェードアウト
-    $('.p-top__slider__item').eq(currentIdx).fadeOut(2000);
-    $('.p-top__slider__item').eq(nextIdx).fadeIn(2000);
-    currentIdx = nextIdx;
-  }
-});
+  });
 });
